2f379e5fa50d11ebe78a4c65de7d37c159c673b5
avarashen <voroshen.av@gmail.com>
2019-04-10 02:33:53 +0300

    master: add app name to express server

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/map-spot-info-hunter-client'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/map-spot-info-hunter-client/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
